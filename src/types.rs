64e599d7b6e3a87f06a090892f113785080825e5
Mike <msutherland@fastmail.com>
2024-04-30 20:21:50 -0700

    idiomatic byte conversions

--- src/types.rs
+++ src/types.rs
@@ -18,24 +18,27 @@ pub struct SR1 {
     pub busy: bool,
 }
 
-impl SR1 {
-    fn from_byte(byte: u8) -> Self {
+impl From<u8> for SR1 {
+    fn from(byte: u8) -> Self {
         Self {
             srp0: (byte & 0b1000_0000) != 0,
             sec: (byte & 0b0100_0000) != 0,
             tb: (byte & 0b0010_0000) != 0,
-            bp: (byte & 0b0001_1100) >> 2u8,
+            bp: (byte & 0b0001_1100) >> 2,
             wel: (byte & 0b0000_0010) != 0,
             busy: (byte & 0b0000_0001) != 0,
         }
     }
-    fn to_byte(&self) -> u8 {
-        (self.srp0 as u8) << 7u8
-            | (self.sec as u8) << 6u8
-            | (self.tb as u8) << 5u8
-            | (self.bp & 0b111) << 2u8
-            | (self.wel as u8) << 1u8
-            | self.busy as u8
+}
+
+impl From<SR1> for u8 {
+    fn from(sr1: SR1) -> Self {
+        (sr1.srp0 as u8) << 7
+            | (sr1.sec as u8) << 6
+            | (sr1.tb as u8) << 5
+            | (sr1.bp & 0b111) << 2
+            | (sr1.wel as u8) << 1
+            | sr1.busy as u8
     }
 }
 
@@ -48,23 +51,26 @@ pub struct SR2 {
     pub qe: bool,
     pub srp1: bool,
 }
-impl SR2 {
-    fn from_byte(byte: u8) -> Self {
+
+impl From<u8> for SR2 {
+    fn from(byte: u8) -> Self {
         Self {
             sus: byte & 0b1000_0000 != 0,
             cmp: byte & 0b0100_0000 != 0,
-            lb: (byte >> 5u8) & 0b111,
+            lb: (byte >> 5) & 0b111,
             qe: byte & 0b0000_0010 != 0,
             srp1: byte & 0b0000_0001 != 0,
         }
     }
+}
 
-    fn to_byte(&self) -> u8 {
-        (self.sus as u8) << 7u8
-            | (self.cmp as u8) << 6u8
-            | (self.lb & 0b111) << 5u8
-            | (self.qe as u8) << 1u8
-            | self.srp1 as u8
+impl From<SR2> for u8 {
+    fn from(sr2: SR2) -> Self {
+        (sr2.sus as u8) << 7
+            | (sr2.cmp as u8) << 6
+            | (sr2.lb & 0b111) << 5
+            | (sr2.qe as u8) << 1
+            | sr2.srp1 as u8
     }
 }
 
@@ -75,16 +81,19 @@ pub struct SR3 {
     pub driver_strength: u8,
     pub wps: bool,
 }
-impl SR3 {
-    fn from_byte(byte: u8) -> Self {
+
+impl From<u8> for SR3 {
+    fn from(byte: u8) -> Self {
         Self {
             hold_or_reset: byte & 0b1000_0000 != 0,
-            driver_strength: (byte >> 5) & 0b11, // Get S22 and S21
+            driver_strength: (byte >> 5) & 0b11, // Capture the two bits for driver_strength
             wps: byte & 0b0000_0100 != 0,
         }
     }
+}
 
-    fn to_byte(&self) -> u8 {
-        (self.hold_or_reset as u8) << 7 | (self.driver_strength & 0b11) << 5 | (self.wps as u8) << 2
+impl From<SR3> for u8 {
+    fn from(sr3: SR3) -> Self {
+        (sr3.hold_or_reset as u8) << 7 | (sr3.driver_strength & 0b11) << 5 | (sr3.wps as u8) << 2
     }
 }
